a97f5132c2d230aac269faa6718ebc26b8acd279
Jhoner Pineda <39860554+Zeroluffs@users.noreply.github.com>
2021-07-26 09:19:54 -0500

    Added drawer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import React, { useState } from "react";
 import "./App.css";
-import CreateKeep from "./components/CreateKeep";
-import Keeps from "./components/Keeps";
-import { Paper } from "@material-ui/core";
 import { ThemeProvider, createTheme } from "@material-ui/core/styles";
-import NavBar from "./components/NavBar";
+import MiniDrawer from "./components/Drawer";
 function App() {
   const [darkMode, setDarkMode] = useState(false);
   const theme = createTheme({
@@ -14,11 +11,17 @@ function App() {
   });
   return (
     <ThemeProvider theme={theme}>
-      <Paper style={{ height: "100vh" }} className="keepContainer">
-        <NavBar check={darkMode} change={() => setDarkMode(!darkMode)} />
-        <CreateKeep darkMode={darkMode} />
-        <Keeps />
-      </Paper>
+      <MiniDrawer
+        darkMode={darkMode}
+        check={darkMode}
+        change={() => setDarkMode(!darkMode)}
+      ></MiniDrawer>
+
+      {/* <Paper style={{ height: "100vh" }} className="keepContainer">
+        <NavBar check={darkMode} change={() => setDarkMode(!darkMode)} /> */}
+      {/* <CreateKeep darkMode={darkMode} /> */}
+      {/* <Keeps />
+      </Paper> */}
     </ThemeProvider>
   );
 }
